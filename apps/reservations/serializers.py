a32f033645337d980547c9d0ceff8a4cf923e006
Daniel Caraballo <danielcaraballoponte@gmail.com>
2024-09-07 16:23:30 -0400

    perf(serializers): add validations to serializers

--- apps/reservations/serializers.py
+++ apps/reservations/serializers.py
@@ -14,6 +14,11 @@ class CustomerSerializer(serializers.ModelSerializer):
         model = Customer
         fields = '__all__'
 
+    def validate_email(self, value):
+        if Customer.objects.filter(email=value).exists():
+            raise serializers.ValidationError('Email is already in use.')
+        return value
+
 
 class RatingSerializer(serializers.ModelSerializer):
     customer = CustomerSerializer(read_only=True)
@@ -21,6 +26,12 @@ class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
         fields = '__all__'
+
+    def validate_rating(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError(
+                'Rating must be between 1 and 5.')
+        return value
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
@@ -33,6 +44,12 @@ class TurnSerializer(serializers.ModelSerializer):
     class Meta:
         model = Turn
         fields = '__all__'
+
+    def validate(self, data):
+        if data['opening_time'] >= data['closing_time']:
+            raise serializers.ValidationError(
+                'Opening time must be before closing time.')
+        return data
 
 
 class TableSerializer(serializers.ModelSerializer):
@@ -49,6 +66,16 @@ class TableAvailabilitySerializer(serializers.ModelSerializer):
         model = TableAvailability
         fields = '__all__'
 
+    def validate(self, data):
+        if TableAvailability.objects.filter(
+            table=data['table'],
+            date=data['date'],
+            turn=data['turn']
+        ).exists():
+            raise serializers.ValidationError(
+                'The table is already available for this date and time.')
+        return data
+
 
 class ReservationsSerializer(serializers.ModelSerializer):
     customer = CustomerSerializer(read_only=True)
@@ -59,3 +86,18 @@ class ReservationsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = '__all__'
+
+    def validate_number_guests(self, value):
+        if value < 0:
+            raise serializers.ValidationError(
+                'Number of guests cannot be negative.')
+        return value
+
+    def validate(self, data):
+        if Reservation.objects.filter(
+            reservation_date=data['reservation_date'],
+            reservation_turn=data['reservation_turn']
+        ).exists():
+            raise serializers.ValidationError(
+                'The table is already reserved for this date and time.')
+        return data
