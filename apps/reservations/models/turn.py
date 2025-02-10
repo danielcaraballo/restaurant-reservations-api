12bac931b0d02ddb30766732b12d0d45f614c720
Daniel Caraballo <danielcaraballoponte@gmail.com>
2025-02-10 16:49:23 -0400

    refactor(models): refactor status in models apps.reservations

--- apps/reservations/models/turn.py
+++ apps/reservations/models/turn.py
@@ -2,11 +2,18 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 
+class TurnStatus(models.TextChoices):
+    ACTIVE = 'active', 'Active'
+    INACTIVE = 'inactive', 'Inactive'
+
+
 class Turn(models.Model):
 
     name = models.CharField('Name', max_length=50, null=False, blank=False)
     opening_time = models.TimeField()
     closing_time = models.TimeField()
+    status = models.CharField(
+        'Status', max_length=20, choices=TurnStatus.choices, default=TurnStatus.ACTIVE)
 
     class Meta:
         managed = True
