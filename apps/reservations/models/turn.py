d1c515a4c40f3d8e01223a9c046ba575cbf9ae91
Daniel Caraballo <danielcaraballoponte@gmail.com>
2025-02-10 17:40:12 -0400

    feat(reservations): add validation in model turn

--- apps/reservations/models/turn.py
+++ apps/reservations/models/turn.py
@@ -8,7 +8,6 @@ class TurnStatus(models.TextChoices):
 
 
 class Turn(models.Model):
-
     name = models.CharField('Name', max_length=50, null=False, blank=False)
     opening_time = models.TimeField()
     closing_time = models.TimeField()
@@ -22,8 +21,24 @@ class Turn(models.Model):
         verbose_name_plural = 'Turns'
 
     def clean(self):
+        """Validaciones antes de guardar el modelo"""
         if self.opening_time >= self.closing_time:
             raise ValidationError('Opening time must be before closing time.')
 
+        overlapping_turns = Turn.objects.filter(
+            opening_time__lt=self.closing_time,
+            closing_time__gt=self.opening_time,
+            status=TurnStatus.ACTIVE  # Solo verifica solapamiento en turnos activos
+        ).exclude(pk=self.pk)
+
+        if overlapping_turns.exists():
+            raise ValidationError(
+                "This turn overlaps with another active turn.")
+
+    def save(self, *args, **kwargs):
+        """Asegura que clean() se ejecute antes de guardar"""
+        self.full_clean()
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.name} ({self.get_status_display()})'
